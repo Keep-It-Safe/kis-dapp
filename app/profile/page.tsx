a2e09253eaf7c94d2d9a734d409b5a249135ce57
Vaibhav <vaibhav.p@xpuls.ai>
2024-03-30 16:58:30 +0530

    add nextui setup

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@nextui-org/button";
+
 export default function Profile() {
     return (
+        <>
         <h1 className="font-bold text-3xl">Profile page</h1>
+        <Button color="primary">
+            Submit
+        </Button>
+        </>
     )
 }
