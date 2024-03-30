cf72ae596be48caaeaba1beefb5b828fb64fef68
Vaibhav <vaibhav.p@xpuls.ai>
2024-03-30 22:41:09 +0530

    change header title

--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,30 @@ import React from "react";
 import { AuroraBackground } from "../components/ui/aurora-background";
 import { NavbarDemo } from "@/components/navbar";
 
-export default function AuroraBackgroundDemo() {
+export default function Home() {
   return (
-    <AuroraBackground>
-        <NavbarDemo/>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
-      >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          {"Let's Keep It Safe"}
-        </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          Store all your documents in one place
-        </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Start now
-        </button>
-      </motion.div>
-    </AuroraBackground>
+      <AuroraBackground>
+        <NavbarDemo />
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative flex flex-col gap-4 items-center justify-center px-4"
+        >
+          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+            {"Let's Keep It Safe"}
+          </div>
+          <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+            Store all your documents in one place
+          </div>
+          <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+            Start now
+          </button>
+        </motion.div>
+      </AuroraBackground>
   );
 }
