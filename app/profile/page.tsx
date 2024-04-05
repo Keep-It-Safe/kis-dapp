675ed8a6f586e9755101684094c89791b40cadc0
Vaibhav <vaibhav.p@xpuls.ai>
2024-04-05 19:34:36 +0530

    add update profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,32 +1,34 @@
 "use client";
 
-import { motion } from "framer-motion";
 import React from "react";
-import { AuroraBackground } from "../../components/ui/aurora-background";
-
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useWallets } from "@privy-io/react-auth";
+import axios from "axios";
 export default function AuroraBackgroundDemo() {
-  return (
-    <AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
-      >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Background lights are cool you know.
-        </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
-        </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
-        </button>
-      </motion.div>
-    </AuroraBackground>
-  );
+    const router = useRouter();
+    const { wallets } = useWallets();
+    const wallet = wallets[0];
+    const [isUniversity, setIsUniversity] = useState<Boolean>(false);
+    const [isLoading, setIsLoading] = useState<Boolean>(false);
+    const [isProfileComplete, setIsProfileComplete] = useState<Boolean>(false);
+    useEffect(() => {
+      setIsLoading(true);
+      axios.get(`/api/findUser?address=${wallet?.address}`).then(
+        (response) => {
+          setIsUniversity(response.data.isUniversity);
+          setIsProfileComplete(response.data.isProfileComplete);
+          if(!isProfileComplete) {
+              router.push('/profile/complete');
+          }
+          console.log(response.data);
+          setIsLoading(false);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }, []);
+  
+    return isLoading?<h1>Loading...</h1>:(isUniversity? <h1>University</h1>:<h1>Student</h1>)
 }
