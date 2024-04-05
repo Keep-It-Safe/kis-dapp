f3f292641900030a2f11f509260e3c877d12e819
Vaibhav <vaibhav.p@xpuls.ai>
2024-04-05 22:11:53 +0530

    add request page

--- components/pending-request.tsx
+++ components/pending-request.tsx
@@ -1,7 +1,47 @@
 "use client";
 
-export default function PendingRequest() {
-    return (
-        <h1>Pending requests</h1>
-    );
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { Button } from "@nextui-org/react";
+
+export default function RaiseRequest() {
+  return (
+    <div className="h-[100vh] flex justify-center items-center flex-col">
+      <div className="mb-[1%]">
+        <h1 className="text-6xl">Pending Requests</h1>
+        <div className="max-w-5xl mx-auto px-8 mt-5">
+          <HoverEffect items={projects} />
+        </div>
+        <div className="flex flex-col-reverse">
+          <Button color="secondary">Submit</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export const projects = [
+  {
+    title: "ID Card",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "",
+  },
+  {
+    title: "Degree",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime",
+    link: "",
+  },
+  {
+    title: "Marksheet",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "",
+  },
+  {
+    title: "Marksheet",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "",
+  }
+];
--- components/raise-request.tsx
+++ components/raise-request.tsx
@@ -1,7 +1,51 @@
 "use client";
 
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { Button } from "@nextui-org/react";
+
 export default function RaiseRequest() {
-    return (
-        <h1>Raise a request</h1>
-    );
+
+  return (
+    <div className="h-[100vh] flex justify-center items-center flex-col">
+      <div className="mb-[1%]">
+        <h1 className="text-6xl">Hello, Dhrupad</h1>
+        <h1 className="mt-10 text-3xl">Raise a document request here</h1>
+        <div className="max-w-5xl mx-auto px-8 mt-5">
+          <HoverEffect
+            items={projects}
+          />
+        </div>
+        <div className="flex flex-col-reverse">
+          <Button color="secondary">Submit</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export const projects = [
+    {
+      title: "ID Card",
+      description:
+        "A technology company that builds economic infrastructure for the internet.",
+      link: "",
+    },
+    {
+      title: "Degree",
+      description:
+        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime",
+      link: "",
+    },
+    {
+      title: "Marksheet",
+      description:
+        "A multinational technology company that specializes in Internet-related services and products.",
+      link: "",
+    },
+    {
+        title: "Marksheet",
+        description:
+          "A multinational technology company that specializes in Internet-related services and products.",
+        link: "",
+      },
+  ];
