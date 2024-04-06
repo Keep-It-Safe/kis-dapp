fc1a9be9b6a818d3bee46c62725664b2b58b44e6
Vaibhav <vaibhav.p@xpuls.ai>
2024-04-06 14:34:03 +0530

    update pending request

--- components/pending-request.tsx
+++ components/pending-request.tsx
@@ -1,27 +1,53 @@
 "use client";
 
-import { HoverEffect } from "@/components/ui/card-hover-effect";
-import { Card, CardBody, Button, CardHeader} from "@nextui-org/react";
+import { Card, CardBody, Button, CardHeader, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Input, useDisclosure } from "@nextui-org/react";
 
 export default function PendingRequest() {
+  const {isOpen, onOpen, onOpenChange, onClose} = useDisclosure();
+  const submitRequest = () => {
+    onClose();
+  }
   return (
     <div className="h-[100vh] flex justify-center items-center flex-col mx-10">
       {projects.map((project, index) => (
-        <Card key={project.title + project.name} className="w-full mt-4 p-3" shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+        <Card className="w-full mt-4 p-3" shadow="sm" key={index}>
           <CardHeader className="justify-between">
-            <div>{project.title}</div>
-            <div>{project.name}</div>
+              <span>{project.title}</span>
+              <span>{project.name}</span>
           </CardHeader>
           <CardBody className="flex flex-row-reverse gap-2">
-            <Button color="success" variant="flat">
-                Approve
+            <Button color="success" variant="flat" onPress={onOpen}>
+              Approve
             </Button>
             <Button color="danger" variant="flat">
-                Reject
+              Reject
             </Button>
           </CardBody>
         </Card>
       ))}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Upload document</ModalHeader>
+              <ModalBody>
+                <Input
+                  type="file"
+                  autoFocus
+                  label="Image"
+                  placeholder="Upload document"
+                  variant="bordered"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={submitRequest}>
+                  Submit and Approve
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
@@ -29,14 +55,14 @@ export default function PendingRequest() {
 export const projects = [
   {
     title: "ID Card",
-    name: "Vaibhav"
+    name: "Vaibhav",
   },
   {
     title: "Gradesheet",
-    name: "Abhishek"
+    name: "Abhishek",
   },
   {
     title: "Letter of Recommendation",
-    name: "Dhrupad Sah"
+    name: "Dhrupad Sah",
   },
 ];
