927250c78091c3541c8d2b4e220a893f47e67d6a
Dhrupad <thedhrupad@gmail.com>
2024-04-06 13:38:14 +0530

    Integration with Smart Contract for Pending Requests done

--- app/profile/student-profile.tsx
+++ app/profile/student-profile.tsx
@@ -1,7 +1,50 @@
 "use client";
 
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { Button } from "@nextui-org/react";
+
 export default function StudentProfile() {
-    return (
-        <h1>Student Profile</h1>
-    )
-}
+  return (
+    <div className="h-[100vh] flex justify-center items-center flex-col">
+      <div className="mb-[1%]">
+        <h1 className="text-6xl">A One Stop For All Your Documents</h1>
+        <div className="max-w-5xl mx-auto px-8 mt-5">
+          <HoverEffect
+            items={projects}
+          />
+        </div>
+        <div className="flex flex-col-reverse">
+          <Button color="secondary">Submit</Button>
+        </div>
+      </div>
+    </div>
+  );
+
+};
+
+export const projects = [
+  {
+    title: "ID Card",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "",
+  },
+  {
+    title: "Degree",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime",
+    link: "",
+  },
+  {
+    title: "Marksheet",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "",
+  },
+  {
+      title: "Marksheet",
+      description:
+        "A multinational technology company that specializes in Internet-related services and products.",
+      link: "",
+    },
+];
--- components/pending-request.tsx
+++ components/pending-request.tsx
@@ -2,8 +2,42 @@
 
 import { HoverEffect } from "@/components/ui/card-hover-effect";
 import { Button } from "@nextui-org/react";
+import { useState, useEffect } from "react";
+import { useKeepItSafeContract } from "@/hooks/useKeepItSafe";
+import { useWallets, usePrivy } from "@privy-io/react-auth";
 
 export default function PendingRequest() {
+  const { keepItSafeContract } = useKeepItSafeContract();
+  const { wallets } = useWallets();
+  const wallet = wallets[0];
+
+  useEffect(()=>{
+    const getStudentsRequests = async() => {
+      if(keepItSafeContract){
+        const requestsData = await keepItSafeContract.getAllRequestsForInstitutes();
+        const updatedRequests = await Promise.all(requestsData.map(async (request: any) => {
+          const studentDetails = await getStudentDetails(request[0]);
+          return {
+            studentDetails,
+            docType: request[1],
+            exists: request[2]
+          };
+        }));
+        console.log(updatedRequests);
+        
+        // setRequests(updatedRequests);
+      }
+    }
+    getStudentsRequests();
+  },[])
+
+  async function getStudentDetails(studentAddress: any) {
+    // Call your contract function to get student details here
+    // Assuming you have a function `getStudentDetails` to fetch student details
+    const studentDetails = await keepItSafeContract.getStudentDetails(studentAddress);
+    return studentDetails;
+  }
+
   return (
     <div className="h-[100vh] flex justify-center items-center flex-col">
       <div className="mb-[1%]">
