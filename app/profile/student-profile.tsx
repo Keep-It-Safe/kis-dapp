b6fadfdfe6b08ce728466fd26ceee8f045e459de
Vaibhav <vaibhav.p@xpuls.ai>
2024-04-07 02:06:01 +0530

    edit profile for student page

--- app/profile/student-profile.tsx
+++ app/profile/student-profile.tsx
@@ -78,10 +78,10 @@ export default function StudentProfile() {
     <div className="h-[50rem] mt-16 w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="text-4xl mt-[15%] sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-5">
-        {/* {`${
+        {`${
           studentDetails ? studentDetails[0] : <CircularProgress />
-        }'s documents`} */}
-        Your documents
+        }'s documents`}
+        {/* Your documents */}
       </div>
       <div className=" text-4xl mt-[1%] flex-start">
         <div>Expires in</div>
